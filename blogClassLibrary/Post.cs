cb21c462970f1d4019199712fa45ca29c815c76b
Sebestyen Orsi <elfromka@gmail.com>
2018-12-15 23:44:24 +0200

    Add project files.

---
+++ blogClassLibrary/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blogClassLibrary
+{
+    class Post
+    {
+        public string Content { get; private set; }
+    }
+}
---
+++ blogClassLibrary/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blogClassLibrary
+{
+    class User
+    {
+        public int Id;
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        private DateTime BirthDate;
+        public string EmailAdress { get; private set; }
+    }
+}
