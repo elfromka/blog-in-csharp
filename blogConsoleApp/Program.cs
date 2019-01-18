235388d562efc13485b7c94b12ea8c4f27badf6a
Sebestyen Orsi <elfromka@gmail.com>
2019-01-18 19:34:47 +0200

    building up the console

--- blogConsoleApp/Program.cs
+++ blogConsoleApp/Program.cs
@@ -9,8 +9,24 @@ namespace blogConsoleApp
 {
     class Program
     {
+         private static void DisplayMenu()
+        {
+            Console.WriteLine("Blog in C#{0}", Environment.NewLine);
+            Console.WriteLine("--------------------------------------------");
+            // display nr of posts here, in the phranteses
+            Console.WriteLine("################## Posts (0) ############### {0}", Environment.NewLine);
+            Console.WriteLine("There are no posts to show at this moment");
+            Console.WriteLine("--------------------------------------------{0}", Environment.NewLine);
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Add a new post");
+            Console.WriteLine("2 - Create a new account");
+            Console.WriteLine("X - Exit");
+        }
+
         static void Main(string[] args)
         {
+            DisplayMenu();
+            Console.ReadLine();
         }
     }
 }
